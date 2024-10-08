10905237d96854dc7f8321de70fa83fd691acd23
Gil Benezer <gil.benezer@gmail.com>
2024-10-08 17:02:51 -0400

    Found that bug was due to incorrect handling of projections in the initial version of the tle_processing.py tles_to_geopandas function

--- src/geopandas_test.py
+++ src/geopandas_test.py
@@ -3,41 +3,28 @@
 
 import matplotlib.pyplot as plt
 from pathlib import Path
+from datetime import datetime, timezone
 import numpy as np
 import pyproj as pyj
 import pandas as pd
 import geopandas as gpd
+import tle_processing as tlp
 
-# constructing the relative data directory path
+# # constructing the relative data directory path
 dirpath = Path(__file__).parent.resolve() / ".." / "data"
-
-# getting the file path of the current starlink positions
-position_filepath = dirpath / "starlink_positions.csv"
 
 # defining a common projection for data
 common_crs = pyj.CRS.from_proj4("+proj=eck4")
 
-# importing the positions as a pandas DataFrame object
-starlink_raw = pd.read_csv(filepath_or_buffer=position_filepath, header=0)
-
-# renaming the columns because there's whitespace in the column names
-# due to how the data was converted to a csv (for now, may encapsulate and modify functions for data conversion)
-starlink_raw = starlink_raw.rename(columns={" Latitude" : "latitude",
-                             " Longitude" : "longitude",
-                             " Altitude" : "altitude"})
-
-# adding the radii to the dataframe
-starlink_raw["radius"] = np.pi * (starlink_raw["altitude"] * np.tan(45.0 / 2.0))**2
-
-# adding the coordinates as a Shapely geometry object to the DataFrame
-starlink_raw["geometry"] = gpd.points_from_xy(x=starlink_raw["longitude"],
-                                              y=starlink_raw["latitude"])
-
 # create a GeoDataFrame object
-starlink = gpd.GeoDataFrame(starlink_raw)
+starlink = tlp.tles_to_geodataframe(input_file_name=dirpath/"starlink_tle_06OCT2024_21_22.txt",
+                                      time=datetime.now(timezone.utc),
+                                      crs_string="4326",
+                                      add_radius=True,
+                                      add_area=True,
+                                      set_crs=True)
 
 # projecting the data to the Eckert IV equal area projection
-starlink = starlink.set_crs(pyj.CRS.from_epsg("4326"))
 starlink = starlink.to_crs(common_crs)
 
 # create a map projection that can be reasoned about
@@ -48,7 +35,7 @@ world_map = world_map.to_crs(common_crs)
 
 # Tested that this map file does actually label the areas that are land
 # so a spatial join will probably be able to get us land area
-print(world_map.head())
+# print(world_map.head())
 
 # showing the plot
 fig, ax = plt.subplots()
