294f98745dcdd017ea60a2214cb7771ad7bf6c2b
Gil Benezer <gil.benezer@gmail.com>
2024-10-11 17:49:07 -0400

    some more testing for the buffer function. will need to reach out to GIS person or find another way to create a circle with GeoPandas

--- src/geopandas_test.py
+++ src/geopandas_test.py
@@ -11,24 +11,48 @@ import geopandas as gpd
 import tle_processing as tlp
 import shapely as sp
 
-# # constructing the relative data directory path
+# # constructing the relative data directory path and the example tle file path
 dirpath = Path(__file__).parent.resolve() / ".." / "data"
+example_tle_txt_file = dirpath / "tle_text_files" / "starlink_tle_06OCT2024_21_22.txt"
 
 # defining a common projection for data
 common_crs = pyj.CRS.from_proj4("+proj=eck4")
 
+# getting the current time
+current_time = datetime.now(timezone.utc)
+
 # create a GeoDataFrame object
-starlink = tlp.tles_to_geodataframe(input_file_name=dirpath/"starlink_tle_06OCT2024_21_22.txt",
-                                      time=datetime.now(timezone.utc),
-                                      buffer_points=False,
-                                      angle=20.0)
+starlink = tlp.tles_to_geodataframe(
+    input_file_name=example_tle_txt_file,
+    time=datetime.now(timezone.utc),
+    buffer_points=False,
+    angle=20.0,
+)
 
 # projecting the data to the Eckert IV equal area projection
 starlink = starlink.set_crs(epsg="4326")
 starlink = starlink.to_crs(common_crs)
 
 # selecting a subset of satellites for testing
-starlink_sample = starlink.head(20).copy(deep=True)
+# starlink_sample = starlink.head(20).copy(deep=True)
+
+# creating just a dataframe of starlink data with one points
+starlink_minimal = tlp.tles_to_dataframe(
+    input_file_name=example_tle_txt_file, time=current_time
+).head(1)
+
+starlink_minimal['geometry'] = gpd.points_from_xy(x=starlink_minimal["Longitude"],
+                                                  y=starlink_minimal["Latitude"])
+
+
+starlink_minimal["geometry"] = starlink_minimal["geometry"].combine(starlink_minimal["Radius"], lambda x,y: x.buffer(y))
+
+starlink_minimal_gpd = gpd.GeoDataFrame(starlink_minimal)
+starlink_minimal_gpd = starlink_minimal_gpd.set_crs(epsg="4326")
+starlink_minimal_gpd = starlink_minimal_gpd.to_crs(common_crs)
+
+
+print(starlink_minimal)
 
 # buffering the points outside of the generation function
 # starlink_sample_buffered = starlink_sample.copy(deep=True)
@@ -39,8 +63,8 @@ starlink_sample = starlink.head(20).copy(deep=True)
 
 # create a map projection that can be reasoned about
 # and project it to the same map projection
-land_filepath = dirpath / "ne_10m_land_scale_rank.zip"
-ocean_filepath = dirpath / "ne_10m_ocean_scale_rank.zip"
+land_filepath = dirpath / "map_data" / "ne_10m_land_scale_rank.zip"
+ocean_filepath = dirpath / "map_data" / "ne_10m_ocean_scale_rank.zip"
 land_map = gpd.read_file(filename=land_filepath)
 ocean_map = gpd.read_file(filename=ocean_filepath)
 land_map = land_map.to_crs(common_crs)
@@ -52,11 +76,16 @@ ocean_map = ocean_map.to_crs(common_crs)
 # print(starlink.head())
 
 
-# showing the plot
+# plotting the map
 fig, ax = plt.subplots()
-land_map.plot(ax=ax, color="#228B22")
-ocean_map.plot(ax=ax, color="#246BCE")
-starlink.plot(ax=ax, color="#C51E3A", markersize = 1)
+# land_map.plot(ax=ax, color="#228B22")
+# ocean_map.plot(ax=ax, color="#246BCE")
+
+# plotting starlink data
+# starlink.plot(ax=ax, color="#C51E3A", markersize = 1)
 # starlink_sample.plot(ax=ax, color="#C51E3A", markersize = 1)
 # starlink_sample_buffered.plot(ax=ax, color="#C51E3A", markersize = 1)
+starlink_minimal_gpd.plot(ax=ax, color="#C51E3A")
+
+# showing the plot
 plt.show()
