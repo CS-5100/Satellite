2a97bf12b7ac1e033c49502ba4e51cde9948aba3
Gil Benezer <gil.benezer@gmail.com>
2024-10-14 14:49:45 -0400

    Made function for creating dataframe from current TLEs so that I could smoothly try to troubleshoot issues with GeoPandas and Shapely. I believe it has to do with proper map projection and converting latitude/longitude measurements to actual cartesian coordinates.

--- src/download_tle.py
+++ src/download_tle.py
@@ -1,11 +1,14 @@
 import requests
+import pandas as pd
 from datetime import datetime, timezone
+from tle_processing import tle_lines_to_lists
+
 
 def download_tle_data(url):
     """
     Downloads TLE data from the given URL and saves it to a text file.
     The filename will automatically include the current date and time.
-    
+
     Parameters:
     - url: The URL to download the TLE data from.
     """
@@ -16,20 +19,108 @@ def download_tle_data(url):
 
         # Get the current date and time to format the filename
         current_time = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
-        # current_time = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
         filename = f"starlink_tle_{current_time}.txt"
 
         # Save the content to a text file
-        with open(filename, 'w') as file:
+        with open(filename, "w") as file:
             file.write(response.text)
 
         print(f"TLE data successfully downloaded and saved to {filename}")
-    
+
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
+
+def download_current_tles_as_list(
+    url="https://celestrak.org/NORAD/elements/supplemental/sup-gp.php?FILE=starlink&FORMAT=tle",
+):
+    """Downloads TLE data from the internet and returns them as a list
+
+    Args:
+        url (str, optional): The url to download TLE data from.
+            Defaults to "https://celestrak.org/NORAD/elements/supplemental/sup-gp.php?FILE=starlink&FORMAT=tle".
+
+    Returns:
+        list | None: Returns the TLE data as a list of strings (each TLE is a set of 3 strings) or None if the GET request failed
+    """
+    try:
+        # Send a GET request to the URL
+        response = requests.get(
+            url
+        )  # may want to put a timeout argument here so that the program does not hang
+        response.raise_for_status()  # Raise an exception for HTTP errors
+
+        tle_split_text = response.text.split("\n")  # split the text into lines
+        raw_tle_lines = [
+            line.strip() for line in tle_split_text
+        ]  # strip the white space from each line
+
+        return raw_tle_lines[
+            :-1
+        ]  # return all the lines but the last one, which is a trailing newline
+
+    # error handling
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+        return None
+
+
+def download_current_tles_to_dataframe():
+    """Downloads TLE data from the internet and returns them as a pandas DataFrame
+
+    Returns:
+        pandas.DataFrame | None: Returns the TLE data as pandas DataFrame object or None if the GET request failed.
+            The columns of the DataFrame object are "Satellite" , "Longitude", "Latitude", and "Altitude".
+            The latitude and longitude are in decimal degrees, the altitude is in kilometers
+    """
+
+    # Get the TLEs from Celestrak as a list if possible
+    try:
+        current_raw_tles = download_current_tles_as_list()
+    except requests.exceptions.RequestException:
+        print("An error was encountered when trying to download the TLEs")
+        return None
+
+    # Ensure each TLE has 3 lines and exit if there's not enough lines
+    if len(current_raw_tles) % 3 != 0:
+        print("The number of lines in the TLEs downloaded is unbalanced")
+        return None
+
+    (
+        satellites,
+        longitudes,
+        latitudes,
+        altitudes,
+        not_implemented_errors,
+        crashed_errors,
+    ) = tle_lines_to_lists(current_raw_tles)
+
+    data = {
+        "Satellite": satellites,
+        "Longitude": longitudes,
+        "Latitude": latitudes,
+        "Altitude": altitudes,
+    }
+
+    # If there were any satellites with NotImplementedError, print them
+    if not_implemented_errors:
+        print(
+            f"NotImplementedErrors encountered for satellites: {', '.join(not_implemented_errors)}"
+        )
+
+    # If there were any satellites that are calculated to have crashed, print them
+    if crashed_errors:
+        print(
+            f"The following satellites were calculated to have crashed: {', '.join(crashed_errors)}"
+        )
+
+    return pd.DataFrame(data)
+
+
+# commented this part out because this will generate a text file in any directory this module is used
 # URL for the satellite positions (Starlink TLE data)
-url = "https://celestrak.org/NORAD/elements/supplemental/sup-gp.php?FILE=starlink&FORMAT=tle"
+# url = "https://celestrak.org/NORAD/elements/supplemental/sup-gp.php?FILE=starlink&FORMAT=tle"
 
 # Call the function to download and save the TLE data with auto-named file
-download_tle_data(url)
+# download_tle_data(url)
--- src/geopandas_test.py
+++ src/geopandas_test.py
@@ -4,88 +4,32 @@
 import matplotlib.pyplot as plt
 from pathlib import Path
 from datetime import datetime, timezone
-import numpy as np
 import pyproj as pyj
-import pandas as pd
 import geopandas as gpd
-import tle_processing as tlp
-import shapely as sp
+import download_tle as dtl
 
-# # constructing the relative data directory path and the example tle file path
+# defining the data directory as a relative path
 dirpath = Path(__file__).parent.resolve() / ".." / "data"
-example_tle_txt_file = dirpath / "tle_text_files" / "starlink_tle_06OCT2024_21_22.txt"
-
-# defining a common projection for data
-common_crs = pyj.CRS.from_proj4("+proj=eck4")
 
 # getting the current time
 current_time = datetime.now(timezone.utc)
 
-# create a GeoDataFrame object
-starlink = tlp.tles_to_geodataframe(
-    input_file_name=example_tle_txt_file,
-    time=datetime.now(timezone.utc),
-    buffer_points=False,
-    angle=20.0,
-)
+# creating a DataFrame object
+starlink_current = dtl.download_current_tles_to_dataframe()
+starlink_gpd = gpd.GeoDataFrame(data=starlink_current,
+                                geometry=gpd.points_from_xy(x=starlink_current["Longitude"],
+                                                            y=starlink_current["Latitude"]),
+                                crs="EPSG:4326")
 
-# projecting the data to the Eckert IV equal area projection
-starlink = starlink.set_crs(epsg="4326")
-starlink = starlink.to_crs(common_crs)
-
-# selecting a subset of satellites for testing
-# starlink_sample = starlink.head(20).copy(deep=True)
-
-# creating just a dataframe of starlink data with one points
-starlink_minimal = tlp.tles_to_dataframe(
-    input_file_name=example_tle_txt_file, time=current_time
-).head(1)
-
-starlink_minimal['geometry'] = gpd.points_from_xy(x=starlink_minimal["Longitude"],
-                                                  y=starlink_minimal["Latitude"])
-
-
-starlink_minimal["geometry"] = starlink_minimal["geometry"].combine(starlink_minimal["Radius"], lambda x,y: x.buffer(y))
-
-starlink_minimal_gpd = gpd.GeoDataFrame(starlink_minimal)
-starlink_minimal_gpd = starlink_minimal_gpd.set_crs(epsg="4326")
-starlink_minimal_gpd = starlink_minimal_gpd.to_crs(common_crs)
-
-
-print(starlink_minimal)
-
-# buffering the points outside of the generation function
-# starlink_sample_buffered = starlink_sample.copy(deep=True)
-# starlink_sample_buffered["geometry"] = starlink_sample_buffered["geometry"].combine(starlink_sample_buffered["Radius"],
-#                                                                                     lambda x, y: x.buffer(y))
-# print(starlink_sample_buffered.head())
-
-
-# create a map projection that can be reasoned about
-# and project it to the same map projection
+# importing map data
 land_filepath = dirpath / "map_data" / "ne_10m_land_scale_rank.zip"
 ocean_filepath = dirpath / "map_data" / "ne_10m_ocean_scale_rank.zip"
 land_map = gpd.read_file(filename=land_filepath)
 ocean_map = gpd.read_file(filename=ocean_filepath)
-land_map = land_map.to_crs(common_crs)
-ocean_map = ocean_map.to_crs(common_crs)
-
-# Tested that this map file does actually label the areas that are land
-# so a spatial join will probably be able to get us land area
-# print(world_map.head())
-# print(starlink.head())
-
 
 # plotting the map
 fig, ax = plt.subplots()
-# land_map.plot(ax=ax, color="#228B22")
-# ocean_map.plot(ax=ax, color="#246BCE")
-
-# plotting starlink data
-# starlink.plot(ax=ax, color="#C51E3A", markersize = 1)
-# starlink_sample.plot(ax=ax, color="#C51E3A", markersize = 1)
-# starlink_sample_buffered.plot(ax=ax, color="#C51E3A", markersize = 1)
-starlink_minimal_gpd.plot(ax=ax, color="#C51E3A")
-
-# showing the plot
+land_map.plot(ax=ax, color="#228B22")
+ocean_map.plot(ax=ax, color="#246BCE")
+starlink_gpd.plot(ax=ax, color="#C51E3A", markersize = 1)
 plt.show()
