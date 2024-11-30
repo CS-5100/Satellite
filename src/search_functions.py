32474324f348371a09fe2317f53f23810c1ca912
Gil Benezer <gil.benezer@gmail.com>
2024-11-30 15:55:35 -0500

    added function signature annotation and docstrings for search functions

--- src/search_functions.py
+++ src/search_functions.py
@@ -3,26 +3,58 @@ import pandas as pd
 import numpy as np
 import time
 import math
+from geopandas import GeoDataFrame
 
 
 def hill_climbing(
-    satellite_gdf,
-    map,
-    new_satellite_column,
-    buffer_radius,
-    equal_distance_epsg,
-    equal_area_epsg,
-    perturbation_distance,
+    satellite_gdf: GeoDataFrame,
+    input_map: GeoDataFrame,
+    new_satellite_column: str,
+    buffer_radius: float,
+    equal_distance_epsg: (str | int),
+    equal_area_epsg: (str | int),
+    perturbation_distance: float,
     num_iterations=10,
     print_progress=False,
     random_seed=None,
 ):
+    """Given a geopandas GeoDataFrame of existing satellites and new candidate satellite positions, and a
+    GeoDataFrame defining a land area to cover, use First-Choice Hill Climbing to improve upon the random
+    candidate satellite positions with respect to land area coverage. Current perturbation logic in modules may
+    introduce candidate satellite positions that have empty or NaN coordinates, so beware of that.
+
+    Args:
+        satellite_gdf (GeoDataFrame): A set of satellite coordinates in GeoDataFrame format, including both a set of
+            existing satellites within a satellite constellation (generated with either gdfp.load_existing_satellites or
+            gdfp.load_existing_satellites) concatenated with a set of new candidate satellites generated with
+            gdfp.generate_new_satellites.
+        input_map (GeoDataFrame): The vector map data defining the geometric shape of the land mass to cover.
+        new_satellite_column (str): The string denoting which GeoDataFrame column contains the boolean variable marking new satellites.
+        buffer_radius (float): the assumed coverage radius of each satellite in meters
+        equal_distance_epsg (str  |  int): an equal distance coordinate reference system specified by an EPSG code, used for accurate positional perturbation calculations
+        equal_area_epsg (str  |  int): an equal area coordinate reference system specified by an EPSG code, used for accurate coverage area intersection calculations
+        perturbation_distance (float): The maximum shift in latitude and longitude a satellite position can be perturbed by, in kilometers
+        num_iterations (int, optional): The number of local search iterations to execute. Defaults to 10.
+        print_progress (bool, optional): Whether or not to print the program's progress towards a more optimal solution to the standard output. Defaults to False.
+        random_seed (int, optional): Whether or not to use a random seed for the random number generator. Not guaranteed to be used internally at this point of development.
+            Defaults to None.
+
+    Returns:
+        (GeoDataFrame, float, list[float], list[float]): A tuple of values
+            - best_gdf (GeoDataFrame): a GeoDataFrame containing the existing satellites within a satellite constellation in addition to new candidate satellite positions that
+                that have a land coverage area equal to or greater than the initial random satellite positions in the original GeoDataFrame
+            - best_land_coverage (float): the total land area covered by all the satellites in the output GeoDataFrame
+            - iteration_time_list (list[float]): the time that each local search iteration took in seconds as a list. Mainly for debugging/trace purposes.
+            - best_coverage_list (float): the land area covered by the best set of existing candidate satellite positions so far at each
+                local search iteration as a list. Mainly for debugging/trace purposes.
+            
+    """
     # create random number generator
     rng = np.random.default_rng(seed=random_seed)
 
     best_gdf = satellite_gdf.copy()
     best_land_coverage = gdfp.calculate_land_coverage(
-        gdf=best_gdf, map=map, buffer_radius=buffer_radius
+        gdf=best_gdf, map=input_map, buffer_radius=buffer_radius
     )
     
     iteration_time_list = []
@@ -46,7 +78,7 @@ def hill_climbing(
 
         # Calculate new land coverage area
         new_land_coverage = gdfp.calculate_land_coverage(
-            gdf=new_gdf, map=map, buffer_radius=buffer_radius
+            gdf=new_gdf, map=input_map, buffer_radius=buffer_radius
         )
 
         if new_land_coverage > best_land_coverage:
@@ -71,19 +103,54 @@ def hill_climbing(
 
 
 def random_restart_simulated_annealing(
-    satellite_gdf,
-    map,
-    new_satellite_column,
-    buffer_radius,
-    equal_distance_epsg,
-    equal_area_epsg,
-    perturbation_distance,
+    satellite_gdf: GeoDataFrame,
+    input_map: GeoDataFrame,
+    new_satellite_column: str,
+    buffer_radius: float,
+    equal_distance_epsg: (str | int),
+    equal_area_epsg: (str | int),
+    perturbation_distance: float,
     num_iterations=100,
     initial_temp=1000,
     cooling_rate=0.95,
     restart_threshold=10,
     random_seed=None,
 ):
+    """Given a geopandas GeoDataFrame of existing satellites and new candidate satellite positions, and a
+    GeoDataFrame defining a land area to cover, use First-Choice Hill Climbing in conjunction with random restarting and
+    simulated annealing to improve upon the random candidate satellite positions with respect to land area coverage.
+    Current perturbation logic in modules may introduce candidate satellite positions that have empty or NaN coordinates,
+    so beware of that.
+
+    Args:
+        satellite_gdf (GeoDataFrame): A set of satellite coordinates in GeoDataFrame format, including both a set of
+            existing satellites within a satellite constellation (generated with either gdfp.load_existing_satellites or
+            gdfp.load_existing_satellites) concatenated with a set of new candidate satellites generated with
+            gdfp.generate_new_satellites.
+        input_map (GeoDataFrame): The vector map data defining the geometric shape of the land mass to cover.
+        new_satellite_column (str): The string denoting which GeoDataFrame column contains the boolean variable marking new satellites.
+        buffer_radius (float): the assumed coverage radius of each satellite in meters
+        equal_distance_epsg (str  |  int): an equal distance coordinate reference system specified by an EPSG code, used for accurate positional perturbation calculations
+        equal_area_epsg (str  |  int): an equal area coordinate reference system specified by an EPSG code, used for accurate coverage area intersection calculations
+        perturbation_distance (float): The maximum shift in latitude and longitude a satellite position can be perturbed by, in kilometers
+        num_iterations (int, optional): The number of local search iterations to execute. Defaults to 100.
+        initial_temp (int, optional): The initial temperature to use for the simulated annealing acceptance probability calculations. Defaults to 1000.
+        cooling_rate (float, optional): The float to multiply the temperature by after each local search iteration. Must be between zero and one for proper functionality.
+            Defaults to 0.95.
+        restart_threshold (int, optional): The threshold for the number of local search iterations to execute without land coverage area improvement
+            before randomly restarting with new candidate satellite positions and resetting the temperature parameter. Defaults to 10.
+        random_seed (int, optional): Whether or not to use a random seed for the random number generator. Not guaranteed to be used internally at this point of development.
+            Defaults to None.
+
+    Returns:
+        (GeoDataFrame, float, list[float], list[float]): A tuple of values
+            - best_gdf (GeoDataFrame): a GeoDataFrame containing the existing satellites within a satellite constellation in addition to new candidate satellite positions that
+                that have a land coverage area equal to or greater than the initial random satellite positions in the original GeoDataFrame
+            - best_land_coverage (float): the total land area covered by all the satellites in the output GeoDataFrame
+            - iteration_times (list[float]): the time that each local search iteration took in seconds as a list. Mainly for debugging/trace purposes.
+            - best_land_coverage_list (float): the land area covered by the best set of existing candidate satellite positions so far at each
+                local search iteration as a list. Mainly for debugging/trace purposes.
+    """
     # create random number generator
     rng = np.random.default_rng(seed=random_seed)
 
@@ -95,7 +162,7 @@ def random_restart_simulated_annealing(
     # initialize the best land coverage section
     # and current land coverage section
     best_land_coverage = gdfp.calculate_land_coverage(
-        gdf=best_gdf, map=map, buffer_radius=buffer_radius
+        gdf=best_gdf, map=input_map, buffer_radius=buffer_radius
     )
     current_land_coverage = best_land_coverage
 
@@ -128,7 +195,7 @@ def random_restart_simulated_annealing(
 
         # Calculate new land coverage area
         new_land_coverage = gdfp.calculate_land_coverage(
-            gdf=new_gdf, map=map, buffer_radius=buffer_radius
+            gdf=new_gdf, map=input_map, buffer_radius=buffer_radius
         )
 
         # Calculate the improvement or deterioration
@@ -174,7 +241,7 @@ def random_restart_simulated_annealing(
             new_candidate_satellite_gdf = gdfp.generate_new_satellites(
                 EPSG=equal_area_epsg,
                 num_satellites=30,
-                input_map=map,
+                input_map=input_map,
                 true_random=False,
                 sample_separate=False,
             )
